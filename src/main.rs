f7510c6a17e4be27cad96d92ef2cac65dbfdf3c0
Neville Dipale <nevilledips@gmail.com>
2020-11-16 21:42:58 +0200

    Try without specifying languages
    Spoiler, it did well

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use lingua::Language::{Afrikaans, English, Sotho, Tsonga, Tswana, Xhosa, Zulu};
+use lingua::Language::{Afrikaans, Sotho, Tswana, Xhosa, Zulu};
 use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
 
 fn main() {
-    let languages = vec![Tswana, English, Afrikaans, Sotho, Zulu, Tsonga, Xhosa];
-    let detector: LanguageDetector = LanguageDetectorBuilder::from_languages(&languages).build();
+    // comment out one of the below
+
+    // try with specific languages
+    // let languages = vec![Tswana, English, Afrikaans, Sotho, Zulu, Tsonga, Xhosa];
+    // let detector: LanguageDetector = LanguageDetectorBuilder::from_languages(&languages).build();
+
+    // try with all languages
+    let detector: LanguageDetector = LanguageDetectorBuilder::from_all_languages().build();
 
     // test 1
-    let detected_language: Option<Language> = detector.detect_language_of("Kgosi ya rona e botlhale thata");
+    let detected_language: Option<Language> =
+        detector.detect_language_of("Kgosi ya rona e botlhale thata");
     println!("The detected language is {:?}", detected_language);
     assert_eq!(detected_language, Some(Tswana));
 
     // test 2
-    let detected_language: Option<Language> = detector.detect_language_of("Morena oa rona o bohlale haholo");
+    let detected_language: Option<Language> =
+        detector.detect_language_of("Morena oa rona o bohlale haholo");
     println!("The detected language is {:?}", detected_language);
     assert_eq!(detected_language, Some(Sotho));
 
@@ -21,12 +29,14 @@ fn main() {
     assert_eq!(detected_language, Some(Afrikaans));
 
     // test 4
-    let detected_language: Option<Language> = detector.detect_language_of("Ukumkani wethu ulumke kakhulu");
+    let detected_language: Option<Language> =
+        detector.detect_language_of("Ukumkani wethu ulumke kakhulu");
     println!("The detected language is {:?}", detected_language);
     assert_eq!(detected_language, Some(Xhosa));
 
     // test 5
-    let detected_language: Option<Language> = detector.detect_language_of("Inkosi yethu ihlakaniphe kakhulu");
+    let detected_language: Option<Language> =
+        detector.detect_language_of("Inkosi yethu ihlakaniphe kakhulu");
     println!("The detected language is {:?}", detected_language);
     assert_eq!(detected_language, Some(Zulu));
 }
